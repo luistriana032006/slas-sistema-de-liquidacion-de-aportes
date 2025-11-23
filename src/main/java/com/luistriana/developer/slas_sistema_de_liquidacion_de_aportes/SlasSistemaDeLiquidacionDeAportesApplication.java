015291fdc94f6c9f23252c0bfb46bc55f5a2a00f
luis miguel triana rueda <luistriana617@gmail.com>
2025-11-23 11:30:23 -0500

    chore: initial commit

---
+++ src/main/java/com/luistriana/developer/slas_sistema_de_liquidacion_de_aportes/SlasSistemaDeLiquidacionDeAportesApplication.java
@@ -0,0 +1,13 @@
+package com.luistriana.developer.slas_sistema_de_liquidacion_de_aportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlasSistemaDeLiquidacionDeAportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlasSistemaDeLiquidacionDeAportesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luistriana/developer/slas_sistema_de_liquidacion_de_aportes/SlasSistemaDeLiquidacionDeAportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luistriana.developer.slas_sistema_de_liquidacion_de_aportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlasSistemaDeLiquidacionDeAportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
